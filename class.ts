df31b1cc39dff1831663985378811a66da44adbd
AshishJadhav <vinodjadhav1845@gmail.com>
2023-05-13 22:19:20 +0530

    typescript program

---
+++ class.ts
@@ -0,0 +1,15 @@
+class student1{
+    rollno:number;
+    name:string;
+    constructor(rollno:number,name:string)
+    {
+        this.rollno=this.rollno;
+        this.name=name;
+        console.log(rollno + " " +name);
+    }
+
+ }  let stu = new student1(10,"farhan")
+    
+
+
+var
