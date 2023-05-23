6b5b06dc317626add1c1b70b9ef8ffe928df584b
AshishJadhav <vinodjadhav1845@gmail.com>
2023-05-23 23:38:00 +0530

    php second file of question 1

---
+++ Assignment1/success.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Login page</title>
+        <body>
+            <h1>Login succesfully</h1>
+            <p>You have logged in succesfully </p>
+        </body>
+    </head>
+</html>
